6bcd4784dca799aeca5bf62916798665cf6df46a
Braidyn <57605773+braidyn-tech@users.noreply.github.com>
2020-05-11 04:04:21 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void getProcessorSerialNumber(char *PSN)
+{
+    int varEAX, varEBX, varECX, varEDX;
+    char str[9];
+    __asm__ __volatile__ ("cpuid"   : "=a" (varEAX), "=b" (varEBX), "=c" (varECX), "=d" (varEDX) : "a" (1));
+    sprintf(str, "%08X", varEAX);
+    sprintf(PSN, "%C%C%C%C-%C%C%C%C", str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7]);
+    __asm__ __volatile__ ("cpuid"   : "=a" (varEAX), "=b" (varEBX), "=c" (varECX), "=d" (varEDX) : "a" (3));
+    sprintf(str, "%08X", varEDX);
+    sprintf(PSN, "%s-%C%C%C%C-%C%C%C%C", PSN, str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7]);
+    sprintf(str, "%08X", varECX);
+    sprintf(PSN, "%s-%C%C%C%C-%C%C%C%C", PSN, str[0], str[1], str[2], str[3], str[4], str[5], str[6], str[7]);
+}
+
+int main()
+{
+     char PSN[30];
+     getProcessorSerialNumber(PSN);
+    printf("%s \n", PSN);
+     return 0;
+}
